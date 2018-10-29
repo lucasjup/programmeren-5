186bc5b8623957c37d91c76620292c7c6a193c3c
lucasjup <lucasjup@live.nl>
2018-10-29 14:29:44 +0100

    New project and added models

---
+++ AwenWebsite/Models/Course.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AwenWebsite.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Location { get; set; }
+
+        public string Description { get; set; }
+
+        public int Participants { get; set; }
+
+        public int MaxParticipants { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public decimal Duration { get; set; }
+
+        public decimal Price { get; set; }
+    }
+}
---
+++ AwenWebsite/Models/CourseRegister.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AwenWebsite.Models
+{
+    public class CourseRegister
+    {
+        public int Id { get; set; }
+
+        public int UId { get; set; }
+
+        public int CourseId { get; set; }
+    }
+}
